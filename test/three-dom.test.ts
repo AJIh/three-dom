33e280e6d0879bb6db38e0c6ab2ef6afb30186a8
youfeng.hao <ajihyf@gmail.com>
2019-01-20 23:43:52 +0800

    chore: update dependencies & fix husky

--- test/three-dom.test.ts
+++ test/three-dom.test.ts
@@ -1,14 +1,14 @@
-import DummyClass from "../src/three-dom"
+import DummyClass from '../src/three-dom';
 
 /**
  * Dummy test
  */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
+    expect(true).toBeTruthy();
+  });
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
-})
+  it('DummyClass is instantiable', () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass);
+  });
+});
